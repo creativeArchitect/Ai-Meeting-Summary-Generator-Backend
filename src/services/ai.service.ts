7ebb5fb51f7a1ea51c3421db13748214fbe391eb
AyushAJS <jangidayush89@gmail.com>
2025-08-17 02:27:18 +0530

    API_KEY error is coming resolved by some addition of  redudent code adding, fix later

--- src/services/ai.service.ts
+++ src/services/ai.service.ts
@@ -1,25 +1,28 @@
 import OpenAI from "openai";
+import dotenv from 'dotenv'
 
-const apikey: string = process.env.OPENAI_API_KEY || "";
-const client = new OpenAI({ 
-    apiKey: apikey,
- })
+dotenv.config();
 
-const generateSummary = async (content: string, instruction: string)=> {
-    try{
-        const prompt = `${instruction}\n\nTranscript:\n${content}`
-        const response = await client.chat.completions.create({
-            model: "gpt-4o-mini", 
-            messages: [ {
-                role: "user",
-                content: prompt,
-            } ]
-        })
-        return response.choices[0]?.message?.content;
-    }catch(err){
-        // console.log("error in gen summary service: ", err);
-    }
-}
+const client = new OpenAI({
+  apiKey: process.env.API_KEY,
+});
 
+const generateSummary = async (content: string, instruction: string) => {
+  try {
+    const prompt = `${instruction}\n\nTranscript:\n${content}`;
+    const response = await client.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+    return response.choices[0]?.message?.content;
+  } catch (err) {
+    // console.log("error in gen summary service: ", err);
+  }
+};
 
 export default generateSummary;
